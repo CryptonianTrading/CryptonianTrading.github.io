a56f1b1c92388f315e1e0647aaa6fea751ee8bdc
CryptonianTrading <38576441+CryptonianTrading@users.noreply.github.com>
2018-04-20 23:39:37 -0400

    Update contact-form.php

--- include/contact-form.php
+++ include/contact-form.php
@@ -30,13 +30,13 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST') {
             
                 //If you don't receive the email, enable and configure these parameters below: 
      
-                //$mail->isSMTP();                                      // Set mailer to use SMTP
-                //$mail->Host = 'mail.yourserver.com';                  // Specify main and backup SMTP servers, example: smtp1.example.com;smtp2.example.com
-                //$mail->SMTPAuth = true;                               // Enable SMTP authentication
-                //$mail->Username = 'SMTP username';                    // SMTP username
-                //$mail->Password = 'SMTP password';                    // SMTP password
-                //$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-                //$mail->Port = 587;                                    // TCP port to connect to 
+                $mail->isSMTP();                                      // Set mailer to use SMTP
+                $mail->Host = 'smtp.gmail.com';                  // Specify main and backup SMTP servers, example: smtp1.example.com;smtp2.example.com
+                $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                $mail->Username = 'smtpcryptonian@gmail.com';                    // SMTP username
+                $mail->Password = '%2$qE3U8';                    // SMTP password
+                $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = 587;                                    // TCP port to connect to 
      
      	        $mail->IsHTML(true);                                    // Set email format to HTML
                 $mail->CharSet = 'UTF-8';
